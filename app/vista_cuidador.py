e87685ad2548557d7600aecfe18964ece11e525c
Saul97319 <diegosaul370@gmail.com>
2026-05-18 19:24:11 -0600

    Eliminacion de archivos basura

--- app/vista_cuidador.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-def app():
-    st.title("🛡️ Vista del Cuidador")
-    st.write("Aquí verás el tablero de alertas (En construcción)")
-
-if __name__ == "__main__":
-    app()
-
